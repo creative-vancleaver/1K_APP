862d571ebd93f165a8767e97dc4c250e0c5b7517
Jacob Van Cleave <vancleave.jacob@gmail.com>
2023-10-04 18:52:13 +0000

    update Django to server single page app.
    update git config in ec2 instance to use my name and email

--- backend_k_app/backend_k_app/urls.py
+++ backend_k_app/backend_k_app/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from . import views
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
@@ -21,9 +22,11 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='index.html')),
+#    path('', TemplateView.as_view(template_name='index.html')),
     path('api/languages/', include('base.urls')),
     path('api/users/', include('users.urls')),
+
+    path('', views.index, name='index'),
 
     # re_path(r'^.*/$', TemplateView.as_view(template_name='index.html')),
     re_path(r'^(?!static)(?:.*)/?$', TemplateView.as_view(template_name='index.html')),
